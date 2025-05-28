555efd2056babdca6e91aaa072aa3e93b527cea2
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-05-28 10:19:46 +0300

    [ci] fix terraform fmt

--- tests/localstack/env/main.tf
+++ tests/localstack/env/main.tf
@@ -1,14 +1,14 @@
 module "dynamodb_tables" {
-    for_each = local.schemas
+  for_each = local.schemas
 
-    source = "./dynamo"
+  source = "./dynamo"
 
-    project              = "godyno-test"
-    hash_key             = each.value.hash_key
-    range_key            = each.value.range_key
-    table_name           = each.value.table_name
-    attributes           = each.value.attributes
-    secondary_index_list = each.value.secondary_indexes
+  project              = "godyno-test"
+  hash_key             = each.value.hash_key
+  range_key            = each.value.range_key
+  table_name           = each.value.table_name
+  attributes           = each.value.attributes
+  secondary_index_list = each.value.secondary_indexes
 
-    shared_tags = local.tags
+  shared_tags = local.tags
 }
